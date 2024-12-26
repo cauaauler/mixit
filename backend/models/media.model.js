263ae92924ccb5e083c34e8d19708cedf8ae5733
Cauã Auler <cauaauler@gmail.com>
2024-12-26 20:50:06 -0300

    first steps

---
+++ backend/models/media.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const mediaSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    releaseDate: {
+        type: String,
+        required: false
+    },
+    rating: {
+        type: Number,
+        required: false
+    }
+});
+
+const Media = mongoose.model('Media', mediaSchema);
+
+export default Media;
