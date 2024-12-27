e7f3db714064cd46a561bcfbede5fb554cd04ef6
Cauã Auler <cauaauler@gmail.com>
2024-12-27 15:06:32 -0300

    backend base working

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import mediaRouter from './routes/media.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allow us to accept json from the body
 
 app.use('/api/media', mediaRouter);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at https://localhost:5000');
+    console.log('Server started at https://localhost:' + PORT);
 });
