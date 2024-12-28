5c6dc2a90edb8e7a85335d15966d0f97f08ceb95
Cauã Auler <cauaauler@gmail.com>
2024-12-28 11:09:00 -0300

    starting the thing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { Button, Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+
 
 function App() {
 	return (
 		<>
-    <Box minH={"100vh"}></Box>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes> 
-			<Button colorScheme="teal">Clique Aqui</Button>
+			<Box minH={"100vh"}>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/create" element={<CreatePage />} />
+				</Routes>
+				<Button colorScheme="teal">Clique Aqui</Button>
+			</Box>
 		</>
 	);
 }
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { Box, Heading } from "@chakra-ui/react";
+
+
+function CreatePage() {
+  return (
+        <Box textAlign="center" py={10}>
+            <Heading>Bem-vindo à Create Page!</Heading>
+        </Box>
+  )
+}
+
+export default CreatePage
